27c854ca589c01fae15ff55865f1334d55eb144a
Tractor83457892472094685745 <101215336+Tractor83457892472094685745@users.noreply.github.com>
2022-03-11 09:10:07 -0600

    now to move it you tilt press a+b to stop

--- main.ts
+++ main.ts
@@ -1,34 +1,41 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
-        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinRight)
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Right, 103)
     }
     if (receivedNumber == 3) {
-        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Back, 103)
     }
     if (receivedNumber == 2) {
-        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinLeft)
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Left, 103)
     }
     if (receivedNumber == 4) {
-        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 101)
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 255)
+    }
+    if (receivedNumber == 5) {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
     }
 })
-input.onButtonPressed(Button.A, function () {
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(3)
+})
+input.onGesture(Gesture.TiltLeft, function () {
     radio.sendNumber(2)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
+    radio.sendNumber(5)
 })
-input.onButtonPressed(Button.B, function () {
+input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(0)
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onGesture(Gesture.LogoDown, function () {
     radio.sendNumber(4)
 })
 basic.showLeds(`
     . # . # .
-    . . . . .
-    # # # # #
-    # # # # #
+    . # . # .
     # . . . #
+    # # # # #
+    . # # # .
     `)
 radio.setGroup(1)
